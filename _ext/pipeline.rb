d5c5cde76e0e91cb5790936f8f375443ad97e592
Bob McWhirter <bob@mcwhirter.org>
2010-09-15 15:36:10 -0400

    Move to using awestruct 0.0.9

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,10 +1,5 @@
-require 'team'
-require 'projects'
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  # extension Awestruct::Extensions::Indexifier.new
-  extension Team.new
-  extension Projects.new
+  extension Awestruct::Extensions::DataDir.new
 end
 
